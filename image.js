82643389e46ce0059ddd1ac04df7a88071dae315
Krystian Florkowski <krystianf06@outlook.com>
2026-02-03 13:12:37 +0100

    13_02_partia_9

--- image.js
+++ image.js
@@ -4,9 +4,9 @@ const fs = require("fs-extra");
 const path = require("path");
 const url = require("url");
 
-const PAGE_URL = 'https://rhus-103.man.poznan.pl/dlibra/results?q=POZ-V&action=SimpleSearchAction&type=-6&p=7&ipp=5000';
-const OUT_FILE = path.resolve(__dirname, 'urls.txt');
-
+const PAGE_URL =
+	"https://rhus-103.man.poznan.pl/dlibra/results?q=POZ-V&action=SimpleSearchAction&type=-6&p=8&ipp=5000";
+const OUT_FILE = path.resolve(__dirname, "urls.txt");
 
 async function fetchHtml(pageUrl) {
 	const resp = await axios.get(pageUrl, {
